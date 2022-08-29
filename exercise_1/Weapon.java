6f96b4490f2115d1db6ca3752873939251203944
centsaavedra <111556782+centsaavedra@users.noreply.github.com>
2022-08-29 10:29:00 +0800

    Add files via upload

---
+++ exercise_1/Weapon.java
@@ -0,0 +1,17 @@
+public class Weapon {
+    // Character attributes
+    String name;
+    String weaponname;
+    int size;
+    String class1;
+    String Description1;
+
+    // Character method
+    public void WeaponDiscription() {
+        System.out.println("Object - " + name);
+        System.out.println("Object Name - " + weaponname);
+        System.out.println("Weight - " + size);
+        System.out.println("Rarity - " + class1);
+        System.out.println("Description - " + Description1);
+    }
+}
